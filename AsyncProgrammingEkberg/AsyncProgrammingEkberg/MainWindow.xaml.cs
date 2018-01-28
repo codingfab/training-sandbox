288ea5592bf09a8a4c6b2219c93aa2afbdb926c2
fbn-mrtnz <username@users.noreply.github.com>
2018-01-28 11:13:45 +0000

    WPF app simple login form - starting point

---
+++ AsyncProgrammingEkberg/AsyncProgrammingEkberg/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace AsyncProgrammingEkberg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.Sleep(10000);
+        }
+    }
+}
